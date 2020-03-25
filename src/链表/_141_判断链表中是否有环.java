0555604e8668c44e027bc366875212249dc8d88e
hjw95588 <1824633692@qq.com>
2020-03-25 18:49:17 +0800

    判断链表是否有环

---
+++ src/链表/_141_判断链表中是否有环.java
@@ -0,0 +1,24 @@
+package 链表;
+
+public class _141_判断链表中是否有环 {
+
+	/**
+	 * https://leetcode-cn.com/problems/linked-list-cycle/
+	 * 思路：快，慢指针
+	 * 双指针 fast一次走两个，slow走一个，当两个相遇则有环
+	 */
+   public boolean hasCycle(ListNode head) {
+	   if(head==null || head.next==null) return false;
+	   
+	   ListNode slow=head;
+	   ListNode fast=head.next;
+	   
+	   while(fast!=null && fast.next!=null){
+		   if(fast==slow) return true;
+		  slow=slow.next;
+		  fast=fast.next.next;
+	   }
+        
+	   return false;
+    }
+}
